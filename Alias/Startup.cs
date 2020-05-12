60f77a495f4ea12911b274c802980cc045fb00b9
Mikant <a.mikant@gmail.com>
2020-05-12 16:36:40 +0300

    google-app-engine: "secure: always"

--- Alias/Startup.cs
+++ Alias/Startup.cs
@@ -1,3 +1,4 @@
+using System;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
@@ -5,6 +6,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Alias.Services;
 using Alias.ViewModels;
+using Microsoft.AspNetCore.HttpOverrides;
 
 namespace Alias {
     public class Startup {
@@ -17,13 +19,29 @@ namespace Alias {
         public void ConfigureServices(IServiceCollection services) {
             services.AddRazorPages();
             services.AddServerSideBlazor();
-            
+
             services.AddSingleton<GameService>();
             services.AddScoped<IndexViewModel>();
             services.AddProtectedBrowserStorage();
+
+            services.Configure<ForwardedHeadersOptions>(options =>
+                options.ForwardedHeaders = ForwardedHeaders.XForwardedFor | ForwardedHeaders.XForwardedProto
+            );
         }
 
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env) {
+            app.UseForwardedHeaders();
+            app.Use(async (context, next) => {
+                if (context.Request.IsHttps || context.Request.Headers["X-Forwarded-Proto"] == Uri.UriSchemeHttps)
+                    await next();
+
+                else {
+                    string queryString = context.Request.QueryString.HasValue ? context.Request.QueryString.Value : string.Empty;
+                    var https = "https://" + context.Request.Host + context.Request.Path + queryString;
+                    context.Response.Redirect(https, true);
+                }
+            });
+
             if (env.IsDevelopment()) {
                 app.UseDeveloperExceptionPage();
 
